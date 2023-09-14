990cdcb328b0bb076ba28a313126b8a2a08443fd
iftekhar mahmud <iftekharmahmud98@gmail.com>
2023-09-14 23:13:46 +0600

    added remaining credit and alert

--- src/Componants/Cart/Cart.jsx
+++ src/Componants/Cart/Cart.jsx
@@ -1,12 +1,15 @@
 import Home from "../Home";
 
 const Cart = ({ selectedCourse, totalCredits }) => {
+
     const remainingCredits = 20 - totalCredits;
 
     return (
         <div>
             <div className="w-[100%]  p-3 shadow-md shadow-slate-300">
-                <p className="text-base font-bold text-blue-500">Credit Hour Remaining {remainingCredits} hr</p>
+                <p className="text-sm font-bold text-blue-500">Credit Hour Remaining  
+                
+                {remainingCredits} hr</p>
                 <hr className="my-4" />
                 <p className="text-base font-bold">Course Name</p>
                 <ol>
